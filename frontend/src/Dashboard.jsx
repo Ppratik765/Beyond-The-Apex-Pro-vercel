8d0afc310140c75dac84b428e306e214a0fad36f
Priyanshu Pratik <95397760+Ppratik765@users.noreply.github.com>
2025-12-30 22:09:12 +0530

    Update Dashboard.jsx

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { Line, Scatter } from 'react-chartjs-2';
 import 'chart.js/auto';
